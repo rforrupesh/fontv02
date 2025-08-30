b24e30c4094b9c758ce24756e5cfac17271b6050
rforrupesh <rupeshkumar4775@gmail.com>
2025-08-30 11:47:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ function addCopyIcons() {
   document.querySelectorAll(".copy-btn").forEach(btn => {
     if (!btn.innerHTML.trim()) {
       btn.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+       <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
              viewBox="0 0 24 24" fill="none" stroke="currentColor"
              stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
